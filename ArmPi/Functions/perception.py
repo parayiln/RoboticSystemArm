bff1adf2897062d4b000d18953b90023a58536eb
parayiln <parayiln@oregonstate.edu>
2022-03-11 08:08:32 -0800

    adding

--- ArmPi/Functions/perception.py
+++ ArmPi/Functions/perception.py
@@ -1,30 +1,35 @@
-#!/usr/bin/pyself.thon3
+#!/usr/bin/python3
 # coding=utf8
+import logging
+logging_format = "%(asctime)s: %(message)s"
 import atexit
-# from logdecorator import log_on_start, log_on_end, log_on_error
-import sys
-sys.path.append('/home/nidhi/RoboticSystemArm/ArmPi/')
-
+from logdecorator import log_on_start, log_on_end, log_on_error
 import cv2
 import time
-import Camera
+import math
+import numpy as np
 import threading
+import sys
+sys.path.append('../Lib/ArmPi/')
+import Camera
 from LABConfig import *
 from ArmIK.Transform import *
 from ArmIK.ArmMoveIK import *
+import HiwonderSDK.Board as Board
 from CameraCalibration.CalibrationConfig import *
 
-if sys.version_info.major == 2:
-    print('Please run this program with python3!')
-    sys.exit(0)
 
 class Perception(object):
-    # @log_on_start(logging.DEBUG, "Perception starting ")
-    # @log_on_error(logging.DEBUG, "Error in Perception")
-    # @log_on_end(logging.DEBUG, "Perception done")
 
-    def __init__(self):
-
+    @log_on_start(logging.DEBUG, "Constructor called ")
+    @log_on_error(logging.DEBUG, "Error in constructor call")
+    @log_on_end(logging.DEBUG, "Constructor finished")
+    def __init__(self, logging_level='INFO'):
+        print("Perception starting")
+        if logging_level == 'INFO':
+            logging.basicConfig(format=logging_format, level=logging.INFO, datefmt="%H:%M:%S")
+        elif logging_level == 'DEBUG':
+            logging.basicConfig(format=logging_format, level=logging.DEBUG, datefmt="%H:%M:%S")
         self.camera = Camera.Camera()
         self.camera.camera_open()
         time.sleep(1) #Time delay for thread in camera class to start generating frames
@@ -55,158 +60,183 @@ class Perception(object):
         self.color_list = []
         self.move_square = False
         self.image = None
-        self.th_p = threading.Thread(target=self.process, args=(), daemon=True)
+        self.th_p = threading.Thread(target=self.run_perception, args=(), daemon=True)
         self.th_p.start()
 
+    @log_on_error(logging.DEBUG, "Can't save, Image empty")
+    def sense(self):
+        img = self.camera.frame
+        if img is not None:
+            self.image = img.copy()
+            self.isRunning = True
+        else:
+            raise IOError("Camera frame empty")
 
 
-    # set be detected color
-    def setTargetColor(self,target_color_new):
-        self.target_color=target_color_new
-        return (True, ())
+    def stop_perception(self):
+        self.isRunning = False
+        print("Stopping perception")
+        self.camera.camera_close()
+        cv2.destroyAllWindows()
 
-    # find self.the maximum area contour
-    # self.the parameter is a list of contours to be compared
-    def getAreaMaxContour(self, contours):
+    @log_on_error(logging.DEBUG, "Can't show, Image empty")
+    def show(self, name='frame', frame=None):
+        if frame is not None:
+            cv2.imshow(name, frame)
+        else:
+            raise IOError("Input frame empty")
+
+
+    def process(self):
+        img = self.image
+        img_copy = self.image.copy()
+        img_h, img_w = img.shape[:2]
+        cv2.line(img, (0, int(img_h / 2)), (img_w, int(img_h / 2)), (0, 0, 200), 1)
+        cv2.line(img, (int(img_w / 2), 0), (int(img_w / 2), img_h), (0, 0, 200), 1)
+
+        if not self.isRunning:
+            return img
+
+        frame_resize = cv2.resize(img_copy, self.size, interpolation=cv2.INTER_NEAREST)
+        frame_gb = cv2.GaussianBlur(frame_resize, (11, 11), 11)
+        # If an area is detected with a recognized object, the area is detected until there are none
+        if self.get_roi and not self.start_pick_up:
+            self.get_roi = False
+            frame_gb = getMaskROI(frame_gb, self.roi, self.size)
+        frame_lab = cv2.cvtColor(frame_gb, cv2.COLOR_BGR2LAB)  # Convert image to LAB space
+
+        color_area_max = None
+        max_area = 0
+        areaMaxContour_max = 0
+
+        if not self.start_pick_up:
+            for i in color_range:
+                if i in self.target_color:
+                    frame_mask = cv2.inRange(frame_lab, color_range[i][0], color_range[i][1])  # Bitwise operations on the original image and mask
+                    opened = cv2.morphologyEx(frame_mask, cv2.MORPH_OPEN, np.ones((6, 6), np.uint8))  # open operation
+                    closed = cv2.morphologyEx(opened, cv2.MORPH_CLOSE, np.ones((6, 6), np.uint8))  # closed operation
+                    contours = cv2.findContours(closed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)[
+                        -2]  # find the outline
+                    areaMaxContour, area_max = self.getAreaMaxContour(contours)  # find the largest contour
+                    if areaMaxContour is not None:
+                        if area_max > max_area:  # find the largest area
+                            max_area = area_max
+                            color_area_max = i
+                            areaMaxContour_max = areaMaxContour
+            if max_area > 2500:  # have found the largest area
+                self.rect = cv2.minAreaRect(areaMaxContour_max)
+                box = np.int0(cv2.boxPoints(self.rect))
+
+                self.roi = getROI(box)  # get roi region
+                self.get_roi = True
+                img_centerx, img_centery = getCenter(self.rect, self.roi, self.size,
+                                                     square_length)  # Get the coordinates of the center of the block
+
+                self.world_x, self.world_y = convertCoordinate(img_centerx, img_centery, self.size)  # Convert to real world coordinates
+
+                cv2.drawContours(img, [box], -1, self.range_rgb[color_area_max], 2)
+                cv2.putText(img, '(' + str(self.world_x) + ',' + str(self.world_y) + ')',
+                            (min(box[0, 0], box[2, 0]), box[2, 1] - 10),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, self.range_rgb[color_area_max], 1)  # draw center point
+
+                distance = math.sqrt(pow(self.world_x - self.last_x, 2) + pow(self.world_y - self.last_y, 2))  # Compare the last coordinates to determine whether to move
+                self.last_x, self.last_y = self.world_x, self.world_y
+                if not self.start_pick_up:
+                    if color_area_max == 'red':  # red max
+                        color = 1
+                    elif color_area_max == 'green':  # green max
+                        color = 2
+                    elif color_area_max == 'blue':  # blue max
+                        color = 3
+                    else:
+                        color = 0
+                    self.color_list.append(color)
+                    # Cumulative judgment
+                    if distance < 0.5:
+                        self.count += 1
+                        self.center_list.extend((self.world_x, self.world_y))
+                        if self.start_count_t1:
+                            self.start_count_t1 = False
+                            self.t1 = time.time()
+                        if time.time() - self.t1 > 1:
+                            self.rotation_angle = self.rect[2]
+                            self.start_count_t1 = True
+                            self.world_X, self.world_Y = np.mean(np.array(self.center_list).reshape(self.count, 2), axis=0)
+                            self.center_list = []
+                            self.count = 0
+                            self.start_pick_up = True
+                    else:
+                        self.t1 = time.time()
+                        self.start_count_t1 = True
+                        self.center_list = []
+                        self.count = 0
+
+                    if len(self.color_list) == 3:  # multiple judgments
+                        # take the average
+                        color = int(round(np.mean(np.array(self.color_list))))
+                        self.color_list = []
+                        if color == 1:
+                            self.detect_color = 'red'
+                            self.draw_color = self.range_rgb["red"]
+                        elif color == 2:
+                            self.detect_color = 'green'
+                            self.draw_color = self.range_rgb["green"]
+                        elif color == 3:
+                            self.detect_color = 'blue'
+                            self.draw_color = self.range_rgb["blue"]
+                        else:
+                            self.detect_color = 'None'
+                            self.draw_color = self.range_rgb["black"]
+            else:
+                if not self.start_pick_up:
+                    self.draw_color = (0, 0, 0)
+                    self.detect_color = "None"
+        if self.move_square:
+            cv2.putText(img, "Make sure no blocks in the stacking area", (15, int(img.shape[0] / 2)),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2)
+
+        cv2.putText(img, "Color: " + self.detect_color, (10, img.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.65, self.draw_color, 2)
+        return img
+
+    def setTargetColor(self, target_color):
+        self.target_color = target_color
+
+
+    # Find the contour with the largest area
+    # argument is a list of contours to compare
+    @staticmethod
+    def getAreaMaxContour(contours):
         contour_area_temp = 0
         contour_area_max = 0
         area_max_contour = None
 
-        for c in contours:  # traversal all self.the contours
-            contour_area_temp = maself.fabs(cv2.contourArea(c))  # calculate self.the countour area
+        for c in contours:  # iterate over all contours
+            contour_area_temp = math.fabs(cv2.contourArea(c))  # Calculate the contour area
             if contour_area_temp > contour_area_max:
                 contour_area_max = contour_area_temp
-                if contour_area_temp > 300:  # only when self.the area is greater self.than 300, self.the contour of self.the maximum area is effective to filter interference
+                if contour_area_temp > 300:  # The contour with the largest area is valid only if the area is greater than 300 to filter out the noise
                     area_max_contour = c
 
-        return area_max_contour, contour_area_max  # return self.the maximum area countour
+        return area_max_contour, contour_area_max  # returns the largest contour
 
-    def start_cam(self):
-        my_camera = Camera.Camera()
-        my_camera.camera_open()
+    def run_perception(self, name='frame'):
+        while True:
+            self.sense()
+            if self.image is not None:
+                frame = self.process()
+                self.show(name, frame)
+                key = cv2.waitKey(1)
+                if key == 27:
+                    self.stop_perception()
+                    break
 
-
-
-
-    def reset(self):
-        self.count = 0
-        self._stop = False
-        self.track = False
-        self.get_roi = False
-        self.center_list = []
-        self.first_move = True
-        self.__target_color = ()
-        self.detect_color = 'None'
-        self.action_finish = True
-        self.start_pick_up = False
-        self.start_count_t1 = True
-
-
-    def start(self):
-        self.reset()
-        self.__isRunning = True
-        print("ColorTracking Start")
-
-
-    # app stop games
-    def stop(self):
-        self._stop = True
-        self__isRunning = False
-        print("ColorTracking Stop")
-
-    # app exit to games
-    def exit(self):
-        self._stop = True
-        self.__isRunning = False
-        print("ColorTracking Exit")
-
-
-# running self.thread
-
-    def process(self,img):
-        img_copy = img.copy()
-        img_h, img_w = img.shape[:2]
-        cv2.line(img, (0, int(img_h / 2)), (img_w, int(img_h / 2)), (0, 0, 200), 1)
-        cv2.line(img, (int(img_w / 2), 0), (int(img_w / 2), img_h), (0, 0, 200), 1)
-        if not __isRunning:
-            return img
-        frame_resize = cv2.resize(img_copy, self.size, interpolation=cv2.INTER_NEAREST)
-        frame_gb = cv2.GaussianBlur(frame_resize, (11, 11), 11)
-        # If it is detected wiself.th a aera recognized object, self.the area will be detected ubtil self.there is no object
-        if get_roi and self.start_pick_up:
-            get_roi = False
-            frame_gb = getMaskROI(frame_gb, self.roi, self.size)
-
-        frame_lab = cv2.cvtColor(frame_gb, cv2.COLOR_BGR2LAB)  # convert self.the image to LAB space
-
-        area_max = 0
-        areaMaxContour = 0
-        if not self.start_pick_up:
-            for i in color_range:
-                if i in __target_color:
-                    detect_color = i
-                    frame_mask = cv2.inRange(frame_lab, color_range[detect_color][0], color_range[detect_color][1])  # maself.thematical operation on self.the original image and mask
-                    opened = cv2.morphologyEx(frame_mask, cv2.MORPH_OPEN, np.ones((6, 6), np.uint8))  # Opening (morphology)
-                    closed = cv2.morphologyEx(opened, cv2.MORPH_CLOSE, np.ones((6, 6), np.uint8))  # Closing (morphology)
-                    contours = cv2.findContours(closed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)[-2]  # find countour
-                    areaMaxContour, area_max = getAreaMaxContour(contours)  # find self.the maximum countour
-            if area_max > 2500:  # find self.the maximum area
-                rect = cv2.minAreaRect(areaMaxContour)
-                box = np.int0(cv2.boxPoints(rect))
-
-                roi = getROI(box) # get roi zone
-                get_roi = True
-
-                img_centerx, img_centery = getCenter(rect, roi, size, square_lengself.th)  # get self.the center coordinates of block
-                world_x, world_y = convertCoordinate(img_centerx, img_centery, size) # convert to world coordinates
-
-
-                cv2.drawContours(img, [box], -1, range_rgb[detect_color], 2)
-                cv2.putText(img, '(' + str(world_x) + ',' + str(world_y) + ')', (min(box[0, 0], box[2, 0]), box[2, 1] - 10),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, range_rgb[detect_color], 1) # draw center position
-                distance = maself.th.sqrt(pow(world_x - last_x, 2) + pow(world_y - last_y, 2)) # compare self.the last coordinate to determine wheself.ther to move
-                last_x, last_y = world_x, world_y
-                track = True
-                #print(count,distance)
-                # cumulative judgment
-                if action_finish:
-                    if distance < 0.3:
-                        center_list.extend((world_x, world_y))
-                        count += 1
-                        if start_count_t1:
-                            start_count_t1 = False
-                            t1 = time.time()
-                        if time.time() - t1 > 1.5:
-                            rotation_angle = rect[2]
-                            start_count_t1 = True
-                            world_X, world_Y = np.mean(np.array(center_list).reshape(count, 2), axis=0)
-                            count = 0
-                            center_list = []
-                            start_pick_up = True
-                    else:
-                        t1 = time.time()
-                        start_count_t1 = True
-                        count = 0
-                        center_list = []
-        return img
 
 if __name__ == '__main__':
 
-    percep =Perception()
-    percep.start_cam()
-    percep.init_val()
-    percep.start()
-    percep.target_color = ('red')
-
+    percept = Perception('DEBUG')
+    percept.setTargetColor(['red','blue','green'])
     while True:
-        img = my_camera.frame
-        if img is not None:
-            frame = img.copy()
-            frame = percep.process()
-            Frame = run(frame)
-            cv2.imshow('Frame', Frame)
-            key = cv2.waitKey(1)
-            if key == 27:
-                break
-    my_camera.camera_close()
-    cv2.destroyAllWindows()
+        key = cv2.waitKey(1)
+        if key == 27:
+            print("main code end")
+            break
